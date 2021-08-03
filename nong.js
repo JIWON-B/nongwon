66ca091c7726393fa63d3b85451e4b4780aac42e
JIWON-B <asdf8087@nate.com>
2021-08-03 16:36:40 +0900

    nongwon web page

--- nong.js
+++ nong.js
@@ -7,6 +7,8 @@ app.use(express.json());
 app.use(express.static('nong'));
 
 
+
+
 // app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 app.listen(9000, function() {
     console.log('Listening on http://localhost:9000/');
